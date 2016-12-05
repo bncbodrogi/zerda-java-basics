b35fc2b8d150a12068fc548b241d401a0fd44f44
BBENCE <bncbodrogi@gmail.com>
2016-12-05 10:04:16 +0100

    player 80% done

--- src/com/greenfox/exams/java/Player.java
+++ src/com/greenfox/exams/java/Player.java
@@ -1,4 +1,20 @@
 package com.greenfox.exams.java;
 
+import java.util.ArrayList;
+
 public class Player {
+    String name;
+    ArrayList<Card> played;
+
+    public int sum(ArrayList<Card> played){
+        int sum = 0;
+        for(Card card : played){
+            sum = sum + card.value;
+        }
+        return sum;
+    }
+
+    public void add(ArrayList<Card> played, Card card){
+        played.add(card);
+    }
 }
