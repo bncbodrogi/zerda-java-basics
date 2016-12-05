cb26405d6c49ca17ab4a1ddfd3b4f8ad7e7d2e78
BBENCE <bncbodrogi@gmail.com>
2016-12-05 09:56:17 +0100

    deck done 50%

--- src/com/greenfox/exams/java/Deck.java
+++ src/com/greenfox/exams/java/Deck.java
@@ -2,6 +2,16 @@ package com.greenfox.exams.java;
 
 import java.util.ArrayList;
 
-public class Deck extends ArrayList {
+public class Deck {
+
+    ArrayList<Card> cards;
+    ArrayList<Card> used;
+
+    public Deck(){
+        for(int i = 0; i > 52; i++){
+            Card card = new Card();
+            cards.add(card);
+        }
+    }
 
 }
