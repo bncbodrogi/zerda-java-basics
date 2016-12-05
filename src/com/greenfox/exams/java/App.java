60528d4e3e7864595aa0766337ee191e453f440e
BBENCE <bncbodrogi@gmail.com>
2016-12-05 10:33:24 +0100

    blackJack 70% done app arond 40%

--- src/com/greenfox/exams/java/App.java
+++ src/com/greenfox/exams/java/App.java
@@ -1,4 +1,12 @@
 package com.greenfox.exams.java;
 
+import javax.swing.*;
+
 public class App {
+        public static void main(String[] args) {
+            BlackJack blackJack =new BlackJack();
+            JFrame frame = new JFrame();
+            
+            frame.add(blackJack);
+        }
 }
--- src/com/greenfox/exams/java/BlackJack.java
+++ src/com/greenfox/exams/java/BlackJack.java
@@ -1,6 +1,34 @@
 package com.greenfox.exams.java;
 
-public class BlackJack {
-    Player house, user;
-    Deck deck;
+import javax.swing.*;
+
+public class BlackJack extends JPanel {
+    JButton reset, drawCard;
+    JPanel user, house, container;
+    JLabel resetLabel, drawCardLabel, userLabel, houseLabel;
+
+    public BlackJack(){
+        container = new JPanel();
+        reset = new JButton();
+        drawCard = new JButton();
+        user = new JPanel();
+        house = new JPanel();
+        resetLabel = new JLabel();
+        drawCardLabel = new JLabel();
+        userLabel = new JLabel();
+        houseLabel = new JLabel();
+
+        container.add(reset);
+        container.add(drawCard);
+        container.add(user);
+        container.add(house);
+
+        reset.add(resetLabel);
+        drawCard.add(drawCardLabel);
+        user.add(userLabel);
+        house.add(houseLabel);
+
+
+    }
+
 }
